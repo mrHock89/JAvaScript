4a4d8a23a9607616cd03e6b1145a8a7fd867523f
koru09 <maonojmondal9900@gmail.com>
2024-05-23 09:43:19 +0530

    added all initial codes

---
+++ 01_basics/practice.js
@@ -0,0 +1,14 @@
+// let score = 'Manoj'
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = " "
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(typeof booleanIsLoggedIn);
+
+
