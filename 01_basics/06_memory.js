70416a0f15d18d363354ab2f6fd43277294460aa
koru09 <manojmondal9900@gmail.com>
2024-05-23 20:33:16 +0530

    learnt about stack and heap memory

--- 01_basics/06_memory.js
+++ 01_basics/06_memory.js
@@ -7,3 +7,22 @@ console.log(myNName);
 
 myNName = "Anirban";
 console.log(myNName);
+
+// Object
+
+let users = {
+    email: "manojmondal9900@gmail.com",
+    userName: "koru09"
+}
+
+console.log(users.email);
+
+users.email = "manoj932@gmail.com"
+console.log(users.email);
+
+// Heap
+
+let userTwo = users;
+
+userTwo.email = "dsfsajfh345@gmail.com"
+console.log(users.email);
