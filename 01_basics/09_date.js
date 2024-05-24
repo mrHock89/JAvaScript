42f58f9bc5f8361e07805d5cd474ad41d399d39f
koru09 <manojmondal9900@gmail.com>
2024-05-24 10:22:49 +0530

    date update

---
+++ 01_basics/09_date.js
@@ -0,0 +1,12 @@
+let myDate = new Date(); // method 1 date decleariation 1
+console.log(myDate);
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+
+console.log(myDate.toTimeString());
+
+console.log(typeof myDate);
