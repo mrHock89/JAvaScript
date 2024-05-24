2597e216be2f8c01f548c0a13f0b0163291c457a
koru09 <manojmondal9900@gmail.com>
2024-05-24 10:34:22 +0530

    ende of the chaper one

--- 01_basics/09_date.js
+++ 01_basics/09_date.js
@@ -1,5 +1,5 @@
 let myDate = new Date(); // method 1 date decleariation 1
-console.log(myDate);
+// console.log(myDate);
 
 console.log(myDate.toString());
 console.log(myDate.toDateString());
@@ -9,4 +9,20 @@ console.log(myDate.toLocaleDateString());
 
 console.log(myDate.toTimeString());
 
-console.log(typeof myDate);
+// console.log(typeof myDate);
+
+// let anotherDate = new Date(2023, 1, 4, 3, 4, 50);
+
+let anotherDate = new Date("01-04-2000")
+// console.log(anotherDate.toString());
+// console.log(anotherDate.toDateString());
+// console.log(anotherDate.toISOString());
+// console.log(anotherDate.toJSON());
+// console.log(anotherDate.toLocaleDateString());
+// console.log(anotherDate.toTimeString());
+
+let myTimeStamp = Date.now()
+let myTimeStamp2 = new Date()
+
+console.log(myTimeStamp)
+console.log(myTimeStamp2.getTime())
