0439be404387f5bf720ebacb0d975c3a43f656fe
mrHock89 <manojmondal9900@gmail.com>
2024-05-25 06:47:11 +0530

    update readme.md file

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,2 +1,26 @@
-console.log(`Hello World`);
-console.log(`World`);
+// Array decleration 
+
+// number array
+const array = [1, 2, 3, 4, 5,];
+
+// Srting array
+const array2 = ["manoj", "raj"];
+
+// using new keyword
+
+const array3 = new Array(1,23, 34,);
+
+
+
+// Array Methods
+
+array.push(6)
+console.log(array);
+array.pop();
+console.log(array);
+
+array.unshift(9)
+console.log(array);
+array.shift(9)
+console.log(array);
+
