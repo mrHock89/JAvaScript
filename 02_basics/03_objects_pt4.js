033d5f13b8cd0e9f7f33465115c900569f4f6806
mrHock89 <manojmondal9900@gmail.com>
2024-05-26 12:08:16 +0530

    update

---
+++ 02_basics/03_objects_pt4.js
@@ -0,0 +1,13 @@
+const myObj = {
+    name: "manoj",
+    age: 21,
+    class: 12,
+    address: "najdhf"
+}
+
+// console.log(myObj);
+// console.log(myObj.name);
+// console.log(myObj["name"]);
+
+const {name: Nmane} = myObj;
+console.log(Nmane);
