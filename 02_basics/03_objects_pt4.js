55b900c07696dfb592f38e48aac005864c30a86f
mrHock89 <manojmondal9900@gmail.com>
2024-05-29 07:55:48 +0530

    update

--- 02_basics/03_objects_pt4.js
+++ 02_basics/03_objects_pt4.js
@@ -1,13 +1,24 @@
-const myObj = {
-    name: "manoj",
-    age: 21,
-    class: 12,
-    address: "najdhf"
+// const myObj = {
+//     name: "manoj",
+//     age: 21,
+//     class: 12,
+//     address: "najdhf"
+// }
+
+// // console.log(myObj);
+// // console.log(myObj.name);
+// // console.log(myObj["name"]);
+
+// const {name: Nmane} = myObj;
+// console.log(Nmane);
+
+const course = {
+    courseName: "JavaScript",
+    price: "1234",
+    courseINstructor: "Myself"
 }
 
-// console.log(myObj);
-// console.log(myObj.name);
-// console.log(myObj["name"]);
-
-const {name: Nmane} = myObj;
-console.log(Nmane);
+const {courseName} = course;
+console.log(courseName);
+const {courseINstructor: teacher} = course;
+console.log(teacher);
