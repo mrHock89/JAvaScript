223831c83dde099fde1f6e32feb96b3a3976a2f1
mrHock89 <manojmondal9900@gmail.com>
2024-06-05 00:44:13 +0530

    update

---
+++ 05_Loop_Iterations/01_do_while_loop.js
@@ -0,0 +1,27 @@
+// // do while loop
+
+// // let i = 0;
+
+// //  do{
+// //     // console.log(i);
+// //     i++;
+// // }while (i <= 10);
+
+// let myArray = ['Flash', 'Batman', 'Superman'];
+
+// let j = 0;
+// do{
+//     console.log(`Value is ${myArray[j]}`);
+//     if(j == 10){
+//         j++;
+//     }
+    
+//     j++;
+    
+// } while(j < myArray.length);
+
+let score = 31;
+do{
+    console.log(score);
+    score++;
+} while(score <= 10);
---
+++ 05_Loop_Iterations/01_while_loop.js
@@ -0,0 +1,14 @@
+// While loop
+let i = 0;
+while (i <= 10) {
+    // console.log(i);
+    i++;
+}
+
+let myArray = ['Flash', 'Batman', 'Superman']
+
+let j = 0;
+while(j < myArray.length){
+    console.log(`Value is ${myArray[j]}`);
+    j++;
+}
