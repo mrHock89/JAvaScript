3995824beab176fc0fadae12abca64fc12844ef1
mrHock89 <manojmondal9900@gmail.com>
2024-07-02 10:11:59 +0530

    update

---
+++ Extra/Extra Reference/nullish_coalescing_op.js
@@ -0,0 +1,3 @@
+// The nullish coalescing operator(??) is a logical operaot tat returns its righ-hand side operand when its left-hand operand is null or ndefined, and otherwise returns its left-hand operabnd.
+let foo = ' / ' ?? 'Hello world';
+console.log(foo);
